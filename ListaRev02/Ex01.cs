128e0cff07ec9389bb49275863d63e2f78542996
Lielly.Natally <93990842+Lielly@users.noreply.github.com>
2022-05-25 20:29:23 +0000

    adicionando ListaRev02/Ex01

--- ListaRev02/Ex01.cs
+++ ListaRev02/Ex01.cs
@@ -3,10 +3,10 @@ using System;
 public class Program {
 
   public static void Main(string[] args) {
-    string s = Console.ReadLine();
-    string [] v = s.Split();
-    int a = int.Parse(v[0]);
-    int b = int.Parse(v[1]);
+    Console.WriteLine("Digite dois valores inteiros");
+     string s = Console.ReadLine(); 
+     int a = int.Parse(s);
+     int b = int.Parse(Console.ReadLine());
     if (a > b) Console.WriteLine($"Maior = {a}");
     if (b > a) Console.WriteLine($"Maior = {b}");
     if (a == b) Console.WriteLine("Números iguais");
