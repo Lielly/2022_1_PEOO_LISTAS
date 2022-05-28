0e79ee9f9156e1128605025347394f2aa05739b7
Lielly.Natally <93990842+Lielly@users.noreply.github.com>
2022-05-28 20:28:54 +0000

    .

---
+++ ListaRev02/Ex04.cs
@@ -0,0 +1,22 @@
+using System;
+
+public class Program{
+  
+  public static void Main(string[] args){
+Console.WriteLine("Digite o primeiro horário no formato hh:mm");
+  string s = Console.ReadLine();
+  string[] v = s.Split(":");
+  int h1 = int.Parse(v[0]);
+  int m1 = int.Parse(v[1]);
+Console.WriteLine("Digite o segundo hor ário no formato hh:mm");
+  string x = Console.ReadLine();
+  string[] y = x.Split(":");
+  int h2 = int.Parse(y[0]);
+  int m2 = int.Parse(y[1]);
+
+  int min = m1 + m2;
+  int h = h1 + h2;
+    if (min >= 60) min - 60 && h + 1
+Console.WriteLine(h + min);
+  }
+}
