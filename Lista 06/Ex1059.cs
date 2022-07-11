c7f4f715600dc6f3646458edf81c74b7c42441ee
Lielly.Natally <93990842+Lielly@users.noreply.github.com>
2022-07-11 01:33:42 +0000

    .

--- Lista 06/Ex1059.cs
+++ Lista 06/Ex1059.cs
@@ -1,1 +1,14 @@
+using System; 
 
+public class Program {
+
+  public static void Main(string[] args) {
+  int x = 1;
+    while (x <= 100) {
+      if (x % 2 == 0) {
+       Console.WriteLine(x); 
+      }
+      x++;
+    }
+  }
+}
--- ListaRev03/Ex10.cs
+++ ListaRev03/Ex10.cs
@@ -1,8 +1,14 @@
-using System; 
+using System;
 
-public class Program {
-
-  public static void Main(string[] args) {
-    
+class Program {
+  public static void Main() {
+    Console.WriteLine("Digite uma frase");
+    string s = Console.ReadLine();
+    int i = 1;
+    while (i <= s.Length) {
+      s = s.Substring(1) + s.Substring(0, 1);
+      Console.WriteLine(s);
+      i++;
+    }
   }
 }
