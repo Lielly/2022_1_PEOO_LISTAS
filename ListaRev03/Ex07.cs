c3b1caf85162a56753d183079f3417f6c6f06242
Lielly.Natally <93990842+Lielly@users.noreply.github.com>
2022-07-11 14:12:50 +0000

    .

--- ListaRev03/Ex07.cs
+++ ListaRev03/Ex07.cs
@@ -5,5 +5,15 @@ class Program {
     Console.WriteLine("Digite uma frase");
     string s = Console.ReadLine();
     string[] v = s.Split();
-  }
+    int n = 0;
+    while (n < v.Length) {
+      int i = n;
+      while(i < v.Length) {
+        Console.Write(v[i] + " ");
+        i++;
+      }
+      Console.WriteLine();
+      n++;
+    }
+  } 
 }
